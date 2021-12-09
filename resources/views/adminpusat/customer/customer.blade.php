eb4184baded1ba386913a189f8d3c2d4be606508
aim-jogja <61280750+aim-jogja@users.noreply.github.com>
2021-12-09 06:41:54 -0800

    Update customer.blade.php
    View-Customer

--- resources/views/adminpusat/customer/customer.blade.php
+++ resources/views/adminpusat/customer/customer.blade.php
@@ -1,5 +1,33 @@
 @extends('adminpusat.app')
 @section('content')
+<style>
+    .customBtn{
+        text-decoration: none;
+        background: #0B3F68;
+        padding: 3px 18px 3px 18px;
+        border-radius: 10px;
+        color: white;
+        font-size: 15px;
+    }
+    .customBtnEdit{
+        text-decoration: none;
+        background: #0B3F68;
+        padding: 3px 23px 3px 23px;
+        border-radius: 10px;
+        color: white;
+        margin-top: 11px;
+        font-size: 15px;
+    }
+    .customBtnDelete{
+        text-decoration: none;
+        background: white;
+        padding: 2px 14px 2px 14px;
+        border-radius: 10px;
+        border: 1px solid red;
+        color: red;
+        font-size: 15px;
+    }
+</style>
 <div class="col-md-12 ms-sm-auto col-lg-12">
       <div class="text-center mb-5 mt-2">
         <h1 class="h2 ">Customer</h1>
@@ -16,7 +44,7 @@
             <h4 class="card-title">Data Customer</h4>
             </div>
             <div class="col-2 text-right">
-            <a href="{{ route('adminpusat.customer.add') }}" class="btn btn-primary">Tambah</a>
+            <a href="{{ route('adminpusat.customer.add') }}" class="customBtn ms-5">Add</a>
             </div>
         </div>
         <div class="row mb-3 table-responsive">
@@ -42,14 +70,14 @@
                     <td>{{ $c->no_telepon }}</td>
                     <td><img src="{{ asset('storage/'.$c->photo) }}" width="150" alt="" class="img-fluid img-thumbnail" ></td>
                     <td align="center">
-                    <div class="btn-group" role="group" aria-label="Basic example">
-                        <a href="{{ route('adminpusat.editcustomer', ['id'=>$c->id]) }}" class="btn btn-warning btn-sm">
-                            <i class="fa fa-edit fa-sm"></i>
+                        <div style="margin-bottom:5px;">
+                        <a href="{{ route('adminpusat.editcustomer', ['id'=>$c->id]) }}" class="customBtnEdit">
+                            Edit
                         </a>
-                        <a href="{{ route('adminpusat.customer.delete', ['id'=>$c->id]) }}" onclick="return confirm('Yakin Hapus data')" class="btn btn-danger btn-sm">
-                            <i class="fa fa-trash-o fa-sm"></i>
+                        </div>
+                        <a href="{{ route('adminpusat.customer.delete', ['id'=>$c->id]) }}" onclick="return confirm('Yakin Hapus data')"  class="customBtnDelete">
+                            Delete
                         </a>
-                    </div>
                     </td>
                 </tr>
                 @endforeach
